99ed18cb9541a044973a7bdc987d8f5a540161d7
narisem <130625075+narisem@users.noreply.github.com>
2023-10-19 19:04:04 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// const btn2 = document.querySelector('.btn2');
+// const btn3 = document.querySelector('.btn3');
+
+// function hello() {
+    //     alert('hi');
+    // };
+
+    
+    document.addEventListener('mouseover', function () {
+        const img = document.querySelector('img'); // クラス名 ".img" ではなく、"img" 要素を取得
+        img.style.display = 'none'; // 要素の表示を非表示に変更
+    });
+    
+    
+
+
+// btn2.addEventListener('click', hello);
+// btn3.addEventListener('click', hello);
